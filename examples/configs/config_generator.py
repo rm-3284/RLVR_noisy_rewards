bfaabbca71bcc4d76f299e29dfcf2e2219611d23
Addison Wu <138899425+addisonwu05@users.noreply.github.com>
2026-03-27 14:08:33 -0400

    added train_global_batch_size

--- examples/configs/config_generator.py
+++ examples/configs/config_generator.py
@@ -17,6 +17,9 @@ grpo:
   num_prompts_per_step: {batch}
   num_generations_per_prompt: {rollout}
 
+policy:
+  train_global_batch_size: {total_batch}
+
 data:
   _override_: true
   max_input_seq_length: ${{policy.max_total_sequence_length}}
